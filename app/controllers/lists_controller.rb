340d74b155d1796c721f4d530579811fae7d512d
kaoriarai06 <132414421+kaoriarai06@users.noreply.github.com>
2023-05-25 13:45:03 +0000

    [finish]3-7 削除機能追加

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -27,6 +27,10 @@ class ListsController < ApplicationController
     redirect_to list_path(list.id)
   end
   
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_a
   private
   def list_params
     params.require(:list).permit(:title, :body, :image)
